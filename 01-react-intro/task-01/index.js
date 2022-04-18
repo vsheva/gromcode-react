3b50aabc8eeb6d062972450cd923706460c3590a
vsheva <valera.sheva@gmail.com>
2022-04-18 20:21:59 +0300

    fix

--- 01-react-intro/task-01/index.js
+++ 01-react-intro/task-01/index.js
@@ -1,9 +1,40 @@
-const rootElement = document.querySelector('#root');
+const rootElem = document.querySelector("#root");
 
-const greetElement = React.createElement(
-    'div',
-    {className: 'greeting'},
-    'Hello, React!'
-);
+const elem = React.createElement(
+    "div",
+    {className: "greeting"},
+    "Hello, Valera! I'm from React!"
+)
 
-ReactDOM.render(greetElement, rootElement);
+ReactDOM.render(elem, rootElem)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const rootElement = document.querySelector('#root');
+//
+// const greetElement = React.createElement(
+//     'div',
+//     {className: 'greeting'},
+//     'Hello, React!'
+// );
+//
+// ReactDOM.render(greetElement, rootElement);
