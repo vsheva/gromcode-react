f7fee4a909bac976ee36a583e40fc13fff445f5b
vsheva <valera.sheva@gmail.com>
2022-04-28 15:41:16 +0300

    fix

--- 03-components-props/task-05/src/Profile.jsx
+++ 03-components-props/task-05/src/Profile.jsx
@@ -4,13 +4,14 @@ import moment from 'moment';
 
 
 
-const formatDate = date => moment(date).format('DD MMM YYYY');
+
 
 function Profile(props) {
+    const formatDate =  moment(props.userData.birthDate).format('DD MMM YYYY');
     return (
         <div className='profile'>
             <div className='profile__name'>{`${props.userData.firstName} ${props.userData.lastName}`}</div>
-            <div className='profile__birth'>{`Was born ${formatDate(props.userData.birthDate)} in ${props.userData.birthPlace}`}</div>
+            <div className='profile__birth'>{`Was born ${formatDate} in ${props.userData.birthPlace}`}</div>
         </div>
     );
 }
