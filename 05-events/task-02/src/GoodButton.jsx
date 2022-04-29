f427ca21ad6f96e7fec47cfc7fc39263290a0f07
vsheva <valera.sheva@gmail.com>
2022-04-29 21:19:17 +0300

    fix

--- 05-events/task-02/src/GoodButton.jsx
+++ 05-events/task-02/src/GoodButton.jsx
@@ -6,7 +6,7 @@ class GoodButton extends React.Component {
     handleClick(e) {
          console.log(e.target.textContent);
          console.log(e);
-        alert("Good job!")
+        alert("Click me!")
     }
 
     render() {
