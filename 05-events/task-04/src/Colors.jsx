da2c6a3d4486538223d923c4bf95d23fef1483f5
vsheva <valera.sheva@gmail.com>
2022-05-01 20:31:40 +0300

    fix

--- 05-events/task-04/src/Colors.jsx
+++ 05-events/task-04/src/Colors.jsx
@@ -6,18 +6,20 @@ const GREEN = '#0f0';
 const BLUE = '#00f';
 
 class Colors extends Component {
-
+constructor(props) {
+    super(props)
+}
     putBackgroundColor(color) {
-        document.body.style.backgroundColor = color
+        document.body.style.backgroundColor = color;
     }
 
 
     render() {
         return (
             <div className="colors">
-               <button style={{backgroundColor:RED}} className="colors_button" onClick={()=>this.putBackgroundColor(RED)} />
-               <button style={{backgroundColor:GREEN}} className="colors_button" onClick={()=>this.putBackgroundColor(GREEN)}/>
-               <button style={{backgroundColor:BLUE}} className="colors_button" onClick={()=>this.putBackgroundColor(BLUE)}/>
+               <button style={{backgroundColor:RED}} className="colors__button" onClick={()=>this.putBackgroundColor(RED)} />
+               <button style={{backgroundColor:GREEN}} className="colors__button" onClick={()=>this.putBackgroundColor(GREEN)}/>
+               <button style={{backgroundColor:BLUE}} className="colors__button" onClick={()=>this.putBackgroundColor(BLUE)}/>
             </div>
         );
     }
