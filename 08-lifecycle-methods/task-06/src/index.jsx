89fdb59751f44fdbda57948d7852cef5da8eb966
vsheva <valera.sheva@gmail.com>
2022-05-07 20:42:21 +0300

    fix

--- 08-lifecycle-methods/task-06/src/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.jsx';
-import './index.scss';
-
-const rootElement = document.querySelector('#root');
-
-ReactDOM.render(<App />, rootElement);
