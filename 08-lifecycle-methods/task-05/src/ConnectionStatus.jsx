5afbad98eb6de7ac8e7ff2d18a0741968e2c5fa2
vsheva <valera.sheva@gmail.com>
2022-05-07 20:39:16 +0300

    fix

--- 08-lifecycle-methods/task-05/src/ConnectionStatus.jsx
+++ 08-lifecycle-methods/task-05/src/ConnectionStatus.jsx
@@ -2,9 +2,8 @@ import React, { Component } from 'react';
 
 class ConnectionStatus extends Component {
   state = {
-      isOnline: 'online',
-    }
-
+    isOnline: 'online',
+  };
 
   componentDidMount() {
     window.addEventListener('online', this.onlineHandler);
@@ -16,43 +15,37 @@ class ConnectionStatus extends Component {
     window.removeEventListener('offline', this.offlineHandler);
   }
 
-  onlineHandler=()=> {
+  onlineHandler = () => {
     this.setState({ isOnline: 'online' });
-  }
+  };
 
-  offlineHandler=()=> {
+  offlineHandler = () => {
     this.setState({ isOnline: 'offline' });
-  }
-
-
-
-
-  // componentDidMount() {
-  //   window.addEventListener('online', this.statusHandler);
-  //   window.addEventListener('offline', this.statusHandler);
-  // }
-  //
-  // componentWillUnmount() {
-  //   window.removeEventListener('online', this.statusHandler);
-  //   window.removeEventListener('offline', this.statusHandler);
-  // }
-  //
-  // statusHandler=(e)=> {
-  //   this.setState({ isOnline: e.type});
-  //   //this.setState({ isOnline: e.type === 'online' });
-  // }
-
+  };
 
   render() {
-
-
     return (
-      <div className={this.state.isOnline ==="offline" ? 'status status_offline' : "status"}>
-        {this.state.isOnline}</div>
+      <div className={this.state.isOnline === 'offline' ? 'status status_offline' : 'status'}>
+        {this.state.isOnline}
+      </div>
     );
   }
 }
 
 export default ConnectionStatus;
 
-
+//refactoring
+// componentDidMount() {
+//   window.addEventListener('online', this.statusHandler);
+//   window.addEventListener('offline', this.statusHandler);
+// }
+//
+// componentWillUnmount() {
+//   window.removeEventListener('online', this.statusHandler);
+//   window.removeEventListener('offline', this.statusHandler);
+// }
+//
+// statusHandler=(e)=> {
+//   this.setState({ isOnline: e.type});
+//   //this.setState({ isOnline: e.type === 'online' });
+// }
