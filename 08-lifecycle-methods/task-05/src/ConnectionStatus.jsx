b4534a742e50d258fcc25b88308ed628d2d05637
vsheva <valera.sheva@gmail.com>
2022-05-07 20:25:56 +0300

    fix

--- 08-lifecycle-methods/task-05/src/ConnectionStatus.jsx
+++ 08-lifecycle-methods/task-05/src/ConnectionStatus.jsx
@@ -6,41 +6,41 @@ class ConnectionStatus extends Component {
     }
 
 
-  componentDidMount() {
-    window.addEventListener('online', this.onlineHandler);
-    window.addEventListener('offline', this.offlineHandler);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('online', this.onlineHandler);
-    window.removeEventListener('offline', this.offlineHandler);
-  }
-
-  onlineHandler() {
-    this.setState({ isOnline: 'online' });
-  }
-
-  offlineHandler() {
-    this.setState({ isOnline: 'offline' });
-  }
-
-
-
-
   // componentDidMount() {
-  //   window.addEventListener('online', this.statusHandler);
-  //   window.addEventListener('offline', this.statusHandler);
+  //   window.addEventListener('online', this.onlineHandler);
+  //   window.addEventListener('offline', this.offlineHandler);
   // }
   //
   // componentWillUnmount() {
-  //   window.removeEventListener('online', this.statusHandler);
-  //   window.removeEventListener('offline', this.statusHandler);
+  //   window.removeEventListener('online', this.onlineHandler);
+  //   window.removeEventListener('offline', this.offlineHandler);
   // }
   //
-  // statusHandler(e) {
-  //   this.setState({ isOnline: e.type});
-  //   //this.setState({ isOnline: e.type === 'online' });
+  // onlineHandler() {
+  //   this.setState({ isOnline: 'online' });
   // }
+  //
+  // offlineHandler() {
+  //   this.setState({ isOnline: 'offline' });
+  // }
+
+
+
+
+  componentDidMount() {
+    window.addEventListener('online', this.statusHandler);
+    window.addEventListener('offline', this.statusHandler);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('online', this.statusHandler);
+    window.removeEventListener('offline', this.statusHandler);
+  }
+
+  statusHandler=(e)=> {
+    this.setState({ isOnline: e.type});
+    //this.setState({ isOnline: e.type === 'online' });
+  }
 
 
   render() {
