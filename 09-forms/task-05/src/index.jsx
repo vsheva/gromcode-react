4d6efd5e3e25f17da1e8dc896a10c4c9a6429598
vsheva <valera.sheva@gmail.com>
2022-05-14 12:08:25 +0300

    fix

--- 09-forms/task-05/src/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import Game from './Game.jsx';
-
-const rootElem = document.querySelector('#root');
-
-ReactDOM.render(<Game />, rootElem);
