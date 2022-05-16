a8235f61d8006d7e3a88224fbc35023ae1d7976c
vsheva <valera.sheva@gmail.com>
2022-05-17 00:09:20 +0300

    fix

---
+++ 10-lifting-state/task-02/src/UserForm.jsx
@@ -0,0 +1,95 @@
+import React, { Component } from 'react';
+
+class UserForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      student: '',
+      occupation: '',
+      about: '',
+    };
+  }
+
+  handleChange = e => {
+    const { name, value, checked, type } = e.target;
+
+    const val = type === 'checkbox' ? checked : value;
+
+    this.setState({
+      [name]: val,
+    });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    //log всех свойств состояния
+      console.log(this.state)
+    this.props.onSubmit(this.state);
+  };
+
+  render() {
+    return (
+      <form className="login-form" onSubmit={this.handleSubmit}>
+        <h1 className="form-title">Profile</h1>
+        <div className="form-control">
+          <label className="form-label" htmlFor="name">
+            Name
+          </label>
+          <input
+            className="form-input"
+            type="text"
+            value={this.state.name}
+            id="name"
+            onChange={this.handleChange}
+            name="name"
+          />
+        </div>
+        <div className="form-control">
+          <label className="form-label" htmlFor="student">
+            Student
+          </label>
+          <input
+            className="form-input"
+            type="checkbox"
+            value={this.state.student}
+            onChange={this.handleChange}
+            id="student"
+            name="student"
+          />
+        </div>
+        <div className="form-control">
+          <label className="form-label" id="occupation" htmlFor="occupation">
+            Occupation
+          </label>
+          <select
+            name="occupation"
+            value={this.state.occupation}
+            onChange={this.handleChange}
+            className="form-input"
+          >
+            <option value="london">London</option>
+            <option value="new-york">New York</option>
+            <option value="coconut">Sidney</option>
+            <option value="mango">Berlin</option>
+          </select>
+        </div>
+        <div className="form-control">
+          <label className="form-label" id="about" htmlFor="about">
+            About
+          </label>
+          <textarea
+            name="about"
+            className="form-input"
+            value={this.state.about}
+            onChange={this.handleChange}
+          />
+        </div>
+        <button className="submit-button" type="submit">
+          Submit
+        </button>
+      </form>
+    );
+  }
+}
+
+export default UserForm;
--- 10-lifting-state/task-02/src/index.jsx
+++ 10-lifting-state/task-02/src/index.jsx
@@ -1,26 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import UsersList from './UsersList.jsx';
+import Game from './Game.jsx';
 
 const rootElem = document.querySelector('#root');
 
-const users = [
-  {
-    name: 'John',
-    age: 34,
-    id: 'id-0',
-  },
-  {
-    name: 'Valerii',
-    age: 15,
-    id: 'id-1',
-  },
-  {
-    name: 'Ivan',
-    age: 21,
-    id: 'id-2',
-  },
-];
-
-ReactDOM.render(<UsersList users={users} />, rootElem);
+ReactDOM.render(<Game />, rootElem);
