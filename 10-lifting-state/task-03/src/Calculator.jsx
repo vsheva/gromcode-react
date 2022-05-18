d475a2f078fcce8f3377925a2b04932288b8e33a
vsheva <valera.sheva@gmail.com>
2022-05-18 12:30:38 +0300

    fix

--- 10-lifting-state/task-03/src/Calculator.jsx
+++ 10-lifting-state/task-03/src/Calculator.jsx
@@ -2,6 +2,26 @@ import React  from 'react';
 import BoilingVerdict from './BoilingVerdict.jsx';
 import TemperatureInput from './TemperatureInput.jsx';
 
+function toCelsius(fahrenheit) {
+  return ((fahrenheit - 32) * 5) / 9;
+}
+
+function toFahrenheit(celsius) {
+  return (celsius * 9) / 5 + 32;
+}
+
+function tryConvert(temperature, convert) {
+  const input = parseFloat(temperature);
+  if (Number.isNaN(input)) {
+    return '';
+  }
+  const output = convert(input);
+  const rounded = Math.round(output * 1000) / 1000;
+  return rounded.toString();
+}
+
+
+
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
@@ -18,30 +38,13 @@ class Calculator extends React.Component {
     this.setState({ scale: 'f', temperature });
   }
 
-  toCelsius(fahrenheit) {
-    return ((fahrenheit - 32) * 5) / 9;
-  }
-
-  toFahrenheit(celsius) {
-    return (celsius * 9) / 5 + 32;
-  }
-
-  tryConvert(temperature, convert) {
-    const input = parseFloat(temperature);
-    if (Number.isNaN(input)) {
-      return '';
-    }
-    const output = convert(input);
-    const rounded = Math.round(output * 1000) / 1000;
-    return rounded.toString();
-  }
 
   render() {
     const scale = this.state.scale;
     const temperature = this.state.temperature;
-    const celsius = scale === 'f' ? this.tryConvert(temperature, this.toCelsius) : temperature;
+    const celsius = scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
     const fahrenheit =
-      scale === 'c' ? this.tryConvert(temperature, this.toFahrenheit) : temperature;
+      scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
 
     return (
       <div>
