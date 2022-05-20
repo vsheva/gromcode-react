57d476847673071a7970b71df643696d078afceb
vsheva <valera.sheva@gmail.com>
2022-05-20 15:36:07 +0300

    fix

--- 11-renew-components/task-03/src/Expand.jsx
+++ 11-renew-components/task-03/src/Expand.jsx
@@ -5,28 +5,22 @@ class Expand extends Component {
     isOpen: false,
   };
 
-  showArrow = () => {
+  toggleMessage = () => {
     this.setState({ isOpen: this.state.isOpen ? false : true });
   };
 
   render() {
     const { children, title } = this.props;
+    const arrow= this.state.isOpen ? <i className="fas fa-chevron-down"></i> : <i className="fas fa-chevron-up"></i>
     return (
       <div className="expand border">
         <div className="expand__header">
           <span className="expand__title">{title}</span>
-          <button
-            isOpen={this.state.isOpen}
-            className="expand__toggle-btn"
-            onClick={this.showArrow}
-          >
-            <i className="fas fa-chevron-up"></i>
+          <button className="expand__toggle-btn" onClick={this.toggleMessage}>
+            {arrow}
           </button>
-        </div>{
-        this.state.isOpen ? (<div className="expand__content">{children}</div>) :null
-      }
-
-
+        </div>
+        {this.state.isOpen ? (<div className="expand__content">{children}</div>) : null}
       </div>
     );
   }
