a31b99e6b26d0ee439cacf374c4ded2dd3df216d
vsheva <valera.sheva@gmail.com>
2022-05-29 20:22:13 +0300

    fix

--- 14-react-hooks/task-03/src/Dimensions.jsx
+++ 14-react-hooks/task-03/src/Dimensions.jsx
@@ -1,6 +1,34 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
-class Dimensions extends Component {
+const Dimensions = () => {
+  const [dimensions, setDimensions] = useState({
+    width: null,
+    height: null,
+  });
+
+  useEffect(() => {
+    const { innerWidth, innerHeight } = window;
+    setDimensions({ width: innerWidth, height: innerHeight });
+
+    const handleResize = event => {
+      const { innerWidth, innerHeight } = event.target;
+      setDimensions({ width: innerWidth, height: innerHeight });
+    };
+
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {window.removeEventListener('resize', handleResize)};
+    }, []);
+
+
+  const { width, height } = dimensions;
+  return <div className="dimensions">{`${width}px - ${height}px`}</div>;
+};
+
+export default Dimensions;
+
+/* class Dimensions extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -12,7 +40,6 @@ class Dimensions extends Component {
   componentDidMount() {
     window.addEventListener('resize', this.handleResize);
     const { innerWidth, innerHeight } = window;
-      console.log(window)
     this.setDimensions({ width: innerWidth, height: innerHeight });
   }
 
@@ -39,3 +66,4 @@ class Dimensions extends Component {
 }
 
 export default Dimensions;
+*/
