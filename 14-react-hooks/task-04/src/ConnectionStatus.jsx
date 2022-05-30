9e1cf0179e61868461956294ec948106c355b151
vsheva <valera.sheva@gmail.com>
2022-05-30 10:49:01 +0300

    fix

--- 14-react-hooks/task-04/src/ConnectionStatus.jsx
+++ 14-react-hooks/task-04/src/ConnectionStatus.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect } from 'react';
+import React, {  useState, useEffect } from 'react';
 
 
 const ConnectionStatus =()=>{
@@ -24,7 +24,7 @@ useEffect(() => {
 
   return (
       <div>
-        <div className ={status==='offline' ? "status__offline" :"status"}>
+        <div className ={status==='offline' ? "status_offline" :"status"}>
           {status}
         </div>
       </div>
@@ -32,58 +32,58 @@ useEffect(() => {
 
 }
 
+ export default ConnectionStatus;
 
 
 
 
 
 
+//
+//
+// class ConnectionStatus extends Component {
+//   constructor(props) {
+//     super(props);
+//
+//     this.state = {
+//       status: "online"
+//     };
+//   }
+//
+//   componentDidMount() {
+//     window.addEventListener("online", this.handleOnline)
+//     window.addEventListener("offline", this.handleOffline)
+//   }
+//
+//
+//
+//   componentWillUnmount() {
+//     window.removeEventListener('online', this.handleOnline)
+//     window.removeEventListener('offline', this.handleOffline)
+//   }
+//
+//
+// handleOnline=()=>{
+//     this.setState({status: "online"});
+//   }
+//
+//   handleOffline=()=>{
+//     this.setState({status: "offline"});
+//   }
+//
+//
+//
+//
+//   render() {
+//     return (
+//       <div>
+//         <div className ={ this.state.status==='offline' ? "status__offline" :"status"}>
+//           {this.state.status}
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+//
 
-
-
-class ConnectionStatus extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      status: "online"
-    };
-  }
-
-  componentDidMount() {
-    window.addEventListener("online", this.handleOnline)
-    window.addEventListener("offline", this.handleOffline)
-  }
-
-
-
-  componentWillUnmount() {
-    window.removeEventListener('online', this.handleOnline)
-    window.removeEventListener('offline', this.handleOffline)
-  }
-
-
-handleOnline=()=>{
-    this.setState({status: "online"});
-  }
-
-  handleOffline=()=>{
-    this.setState({status: "offline"});
-  }
-
-
-
-
-  render() {
-    return (
-      <div>
-        <div className ={ this.state.status==='offline' ? "status__offline" :"status"}>
-          {this.state.status}
-        </div>
-      </div>
-    );
-  }
-}
-
-export default ConnectionStatus;
 
